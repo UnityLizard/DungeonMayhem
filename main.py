05c3f4c7542760bf28f46528e7af675eee75c941
UnityLizard <ivo3948@gmail.com>
2024-02-15 16:24:50 +0200

    art for characters added

--- main.py
+++ main.py
@@ -27,6 +27,37 @@ def draw_bg():
 def draw_panel():
     screen.blit(img_panel, (0, screen_height - bottom_panel))
 
+#fighter class
+class Fighter():
+	def __init__(self, x, y, name, max_hp, strength, potions):
+		self.name = name
+		self.max_hp = max_hp
+		self.hp = max_hp
+		self.strength = strength
+		self.alive = True
+		self.image = pygame.image.load(f'img/characters/{self.name}.png')
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+
+	def draw(self):
+		screen.blit(self.image, self.rect)
+            
+knight1 = Fighter(350, 360, 'king_arthur', 0, 0, 0)
+knight2 = Fighter(280, 360, 'bedevere', 0, 0, 0)
+knight3 = Fighter(210, 360, 'lancelot', 0, 0, 0)
+knight4 = Fighter(140, 360, 'galahad', 0, 0, 0)
+knight5 = Fighter(70, 360, 'robin', 0, 0, 0)
+
+enemy1 = Fighter(420, 360, 'french', 0, 0, 0)
+enemy2 = Fighter(490, 360, 'ni_knight_small', 0, 0, 0)
+enemy3 = Fighter(560, 360, 'ni_knight_big', 0, 0, 0)
+enemy4 = Fighter(630, 360, 'three_headed_knight', 0, 0, 0)
+enemy5 = Fighter(700, 385, 'tim_enchanter', 0, 0, 0)
+enemy6 = Fighter(820, 375, 'black_knight', 0, 0, 0)
+enemy7 = Fighter(700, 300, 'killer_rabbit', 0, 0, 0)
+enemy8 = Fighter(420, 360, 'black_beast', 0, 0, 0)
+
 run = True
 while run:
 
@@ -34,6 +65,21 @@ while run:
 
     draw_bg()
     draw_panel()
+
+    knight1.draw()
+    knight2.draw()
+    knight3.draw()
+    knight4.draw()
+    knight5.draw()
+
+    enemy1.draw()
+    enemy2.draw()
+    enemy3.draw()
+    enemy4.draw()
+    enemy5.draw()
+    enemy6.draw()
+    enemy7.draw()
+    enemy8.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
